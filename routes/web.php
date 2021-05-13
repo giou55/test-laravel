fa984c022e466663769697f216b2ce89d81196f2
Giorgos Giourmetakis <giorgosggg@gmail.com>
2021-05-14 00:22:05 +0300

    a small change

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,6 @@ use App\Http\Controllers\PostsController;
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
-
-fhfhfghfghfghfgh fghfgh
 |
 */
 
